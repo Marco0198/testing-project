160305c273308cb20be07fb9d514a2feb69dfb49
Marco0198 <219049505@mycput.ac.za>
2021-10-15 11:37:38 +0200

    same change in the auth service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,6 +28,7 @@ class AuthServiceProvider extends ServiceProvider
       //  if (! $this->app->routesAreCached()) {
             Passport::routes();
         // }
+        Passport::loadKeysFrom('/secret-keys/oauth');
 
     }
 }
