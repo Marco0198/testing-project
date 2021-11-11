8a864fcd8ac4a34978d38b241bd75f9cdcaf02c4
Marco0198 <219049505@mycput.ac.za>
2021-11-11 11:22:46 +0200

    same change on the reset password and forget password controller

--- app/Http/Controllers/Auth/UserRegistrationController.php
+++ app/Http/Controllers/Auth/UserRegistrationController.php
@@ -58,8 +58,7 @@ class UserRegistrationController extends Controller
             $token =  $user->createToken('pass')->accessToken;
             return response()->json(['success' => true, "message" => 'login successfully', 'token' => $token,]);
         } else
-            return response()->json([
-                "success" => false, "message" => 'The given data was invalid', "password" => 'wrong password'
+            return response()->json(["success" => false, "errors" => 'The given data was invalid', "password" => 'wrong password'
             ]);
     }
 
