dd3e6b1751f3261aab690a53dcb2c72ab3f077a6
Marco0198 <219049505@mycput.ac.za>
2021-12-08 20:45:02 +0200

    same changes

--- app/Http/Controllers/Auth/UserRegistrationController.php
+++ app/Http/Controllers/Auth/UserRegistrationController.php
@@ -110,7 +110,7 @@ class UserRegistrationController extends Controller
     public function profileUpdate(Request $request)
     {
         //validation rules
-        // $id = Auth::id();
+        $id = Auth::id();
         $request->validate(['name' => 'string|max:255',
             'email' => [
                 'email', Rule::unique('users')->ignore($id),
