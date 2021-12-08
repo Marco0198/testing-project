07b2f736075a1c4c8f8c110db473c7a842f64298
Marco0198 <219049505@mycput.ac.za>
2021-12-08 22:28:20 +0200

    same changes

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 Route::get('/user', [UserRegistrationController::class,'getUser'])->middleware('auth:api');
 Route::put('/change_password', [UserRegistrationController::class,'changePassword'])->middleware('auth:api');
-Route::put('/profile_update/{user}', [UserRegistrationController::class, 'profileUpdate'])->middleware('auth:api');
+Route::put('/profile_update/{client_id}', [UserRegistrationController::class, 'profileUpdate'])->middleware('auth:api');
 
 //Route::resource('/task', TaskController::class);
 Route::get('/task/{task}', [TaskController::class,'getTaskById']);
